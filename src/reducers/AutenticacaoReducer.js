6485f8b30c54f4cb34d6106968bff98655b83f71
kairoLamarca <lamarca.kairo@gmail.com>
2018-04-02 13:44:26 -0300

    mapeando estados do redux e encaminhando via props para o FormLogin
    mapeando estados do redux e encaminhando via props para o FormLogin. Utilizando os Reducers.

---
+++ src/reducers/AutenticacaoReducer.js
@@ -0,0 +1,10 @@
+const INITIAL_STATE = {
+    nome: '',
+    email: 'kairo@teste.com',
+    senha: '12345'
+};
+
+//exporta uma função
+export default (state = INITIAL_STATE, action) => {
+    return state;
+};
