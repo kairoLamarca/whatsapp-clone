36ceafdc0baff7debf82c8c9820d921873086427
kairoLamarca <lamarca.kairo@gmail.com>
2018-04-04 13:48:26 -0300

    Configurando o firebase dentro do App
    Configurando o firebase dentro do App

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import firebase from 'firebase';
 
 import Routes from './Routes';
 import reducers from './reducers';//ele ja está importando o index
 
-export default props => (
-    <Provider store={createStore(reducers)}>
-        <Routes />
-    </Provider>
-);
+class App extends Component {
+
+    componentWillMount() {
+        // Initialize Firebase
+        let config = {
+            apiKey: "AIzaSyDcYprcfnZsQGWeWElhwfUQ-OqlrqFPIvY",
+            authDomain: "whatsapp-clone-b269d.firebaseapp.com",
+            databaseURL: "https://whatsapp-clone-b269d.firebaseio.com",
+            projectId: "whatsapp-clone-b269d",
+            storageBucket: "whatsapp-clone-b269d.appspot.com",
+            messagingSenderId: "686760846939"
+        };
+        firebase.initializeApp(config);
+    }
+
+    render() {
+        return (
+            <Provider store={createStore(reducers)}>
+                <Routes />
+            </Provider>
+        );
+    }
+}
+
+export default App;
