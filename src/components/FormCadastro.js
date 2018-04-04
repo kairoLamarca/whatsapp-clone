7bf013f663f8dc57f59931acd495f9e853e28d0e
kairoLamarca <lamarca.kairo@gmail.com>
2018-04-04 12:50:31 -0300

    inclusão de imagem de background
    inclusão de imagem de background

--- src/components/FormCadastro.js
+++ src/components/FormCadastro.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, Image } from 'react-native';
 import { connect } from 'react-redux';
 import { modificaEmail, modificaSenha, modificaNome } from '../actions/AutenticacaoActions';//Action Creators
 
 const formCadastro = props => (
-    <View style={{ flex: 1, padding: 10 }}>
-        <View style={{ flex: 4, justifyContent: 'center' }}>
-            {/* O redux envio as propriedades por props */}
-            <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaNome(texto)} />
-            <TextInput value={props.email} ceholder="E-mail" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaEmail(texto)} />
-            <TextInput secureTextEntry value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaSenha(texto)} />
+    <Image style={{ flex: 1, width: null }} source={require('../imgs/bg.png')}>
+        <View style={{ flex: 1, padding: 10 }}>
+            <View style={{ flex: 4, justifyContent: 'center' }}>
+                {/* O redux envio as propriedades por props */}
+                <TextInput value={props.nome} placeholder="Nome" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaNome(texto)} />
+                <TextInput value={props.email} ceholder="E-mail" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaEmail(texto)} />
+                <TextInput secureTextEntry value={props.senha} placeholder="Senha" style={{ fontSize: 20, height: 45 }} onChangeText={texto => props.modificaSenha(texto)} />
+            </View>
+            <View style={{ flex: 1 }}>
+                <Button title="Cadastrar" color="#115E54" onPress={() => false} />
+            </View>
         </View>
-        <View style={{ flex: 1 }}>
-            <Button title="Cadastrar" color="#115E54" onPress={() => false} />
-        </View>
-    </View>
+    </Image>
 );
 
 const mapStateToPros = state => (
