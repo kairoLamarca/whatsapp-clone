f2936f6527d5683b86b42c4403d80f9c74671067
kairoLamarca <lamarca.kairo@gmail.com>
2018-04-10 14:07:44 -0300

    Desenvolvimento dos components Contatos e Conversas e importação dentro do Principal
    Desenvolvimento dos components Contatos e Conversas e importação dentro do Principal, colocando-os na tab view

--- src/components/Principal.js
+++ src/components/Principal.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
 import { TabViewAnimated, SceneMap } from 'react-native-tab-view';
+
 import TabBarMenu from './TabBarMenu';
+import Conversas from './Conversas';
+import Contatos from './Contatos';
 
 const initialLayout = {
     height: 0,
     width: Dimensions.get('window').width,
 };
-
-const Conversas = () => <View style={[styles.container, { backgroundColor: '#ff4081' }]} />;
-const Contatos = () => <View style={[styles.container, { backgroundColor: '#673ab7' }]} />;
 
 export default class Principal extends Component {
     state = {
