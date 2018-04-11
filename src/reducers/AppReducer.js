6c3d72ced734c8bca5f4eb42f9bfabccee11c9d2
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-04-10 22:07:19 -0300

    Mapeando estados do Redux e encaminhando via Props para o AdicionarContato
    Mapeando estados do Redux e encaminhando via Props para o AdicionarContato

--- src/reducers/AppReducer.js
+++ src/reducers/AppReducer.js
@@ -1,4 +1,6 @@
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+    adiciona_contato_email: 'teste@teste.com'
+};
 
 export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
