990fa39fa1f3520661350a4452ba3df3641c157b
Kairo Lamarca <lamarca.kairo@gmail.com>
2018-04-15 13:06:37 -0300

    criação do mapStateToProps dentro de Contatos
    criação do mapStateToProps dentro de Contatos

--- src/components/Contatos.js
+++ src/components/Contatos.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 import { connect } from 'react-redux';
 import { contatosUsuarioFetch } from '../actions/AppActions';
+import _ from 'lodash';
 
 class Contatos extends Component {
 
@@ -18,4 +19,12 @@ class Contatos extends Component {
     }
 }
 
-export default connect(null, { contatosUsuarioFetch })(Contatos);
+const mapStateToProps = state => {
+    const contatos = _.map(state.ListaContatosReducer, (val, uid) => {
+        return { ...val, uid };
+    });
+    console.log(contatos);
+    return {};
+};
+
+export default connect(mapStateToProps, { contatosUsuarioFetch })(Contatos);
