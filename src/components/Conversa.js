8d1dd5dc5993ffaf13debb541e85dcb0d06ad03f
kairoLamarca <lamarca.kairo@gmail.com>
2018-04-18 13:29:10 -0300

    recuperando as msgs e mostrando na tela com ListView
    recuperando as msgs e mostrando na tela com ListView e utilizando componentWillReceiveProps para atualizar os dados quando os mesmo sofrerem alterações

--- src/components/Conversa.js
+++ src/components/Conversa.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import _ from 'lodash';
-import { View, TextInput, Image, TouchableHighlight } from 'react-native';
+import { View, Text, TextInput, Image, TouchableHighlight, ListView } from 'react-native';
 import { modificaMensagem, enviarMensagem, conversaUsuarioFetch } from '../actions/AppActions';
 
 class Conversa extends Component {
 
     componentWillMount() {
-        this.props.conversaUsuarioFetch(this.props.contatoEmail)
+        this.props.conversaUsuarioFetch(this.props.contatoEmail);
+        this.criaFonteDeDados(this.props.conversa);
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.criaFonteDeDados(nextProps.conversa);
+    }
+
+    criaFonteDeDados(conversa) {
+        const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.dataSource = ds.cloneWithRows(conversa)
     }
 
     _enviarMensagem() {
@@ -16,10 +26,27 @@ class Conversa extends Component {
         this.props.enviarMensagem(mensagem, contatoNome, contatoEmail);
     }
 
+    renderRow(texto) {
+        return (
+            <View>
+                <Text>{texto.mensagem}</Text>
+                <Text>{texto.tipo}</Text>
+            </View>
+        );
+    }
+
     render() {
         return (
             <View style={{ flex: 1, marginTop: 50, backgroundColor: '#eee4dc', padding: 10 }}>
-                <View style={{ flex: 1, paddingBottom: 20 }}></View>
+                <View style={{ flex: 1, paddingBottom: 20 }}>
+
+                    <ListView
+                        enableEmptySections
+                        dataSource={this.dataSource}
+                        renderRow={this.renderRow}
+                    />
+
+                </View>
                 <View style={{ flexDirection: 'row', height: 60 }}>
                     <TextInput
                         value={this.props.mensagem}
@@ -42,8 +69,6 @@ const mapStateToProps = state => {
     const conversa = _.map(state.ListaConversaReducer, (val, uid) => {
         return { ...val, uid };
     });
-
-    console.log(conversa);
 
     return ({
         conversa,
